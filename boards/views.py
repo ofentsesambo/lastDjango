9118a4803268a0590f6fe597ebaae05ab2b10686
ofentsesambo <ofentsesambo@gmail.com>
2020-08-05 20:39:16 +0200

    test.py is giving me a problem

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
 
-def home(requets):
-    return HttpResponse('Hello, World')
+# def home(requets):
+#     return HttpResponse('Hello, World')
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+    
+
+    # for board in boards:
+    #     boards_names.append(board.name)
+
+    #     response_html = '<br>'.join(boards_names)
+
+    #     return HttpResponse(response_html)
